07f18ea9f5fd6e7a878fa55a6bc2fbb962f50d83
Matthew Wright <mwright137@gmail.com>
2021-02-23 13:38:21 -0500

    module 3

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,4 +1,6 @@
 from load_data import load_sensor_data
+from house_info import HouseInfo
+
 # Runner script for all modules
 
 
@@ -16,6 +18,10 @@ print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
 
 # Module 3 code here:
+house_info = HouseInfo(data)
+test_area = 1
+recs = house_info.get_data_by_area("id", rec_area=test_area)
+print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 
 # Module 4 code here:
 
